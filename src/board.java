c8876eb70d31ae22c7c8aa18cf8a1bafc5a160ea
abamba <anais.bamba@outlook.fr>
2016-11-05 21:55:19 +0100

    he's got experience

---
+++ src/board.java
@@ -0,0 +1,7 @@
+
+public class board {
+	int[][] brd = new int[4][4]; 
+	int padding = 10, block = 100, dim = padding*(brd.length+1)+block*brd.length; 
+}
+
+//public void setup() { size(dim, dim); }

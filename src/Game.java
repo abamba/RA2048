cd8e2ead63e074fc818f026326ffdb2cf22b95cf
abamba <anais.bamba@outlook.fr>
2016-11-05 22:29:01 +0100

    i will end you

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,7 @@ public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		board bd = new board(); /* claoker-san was here */
+		board bd = new board(); /* let's kill it anyway */
 	}
 
 }

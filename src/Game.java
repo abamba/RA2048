c2e1633409f76c73b05c01c87cca11f905e625e3
abamba <anais.bamba@outlook.fr>
2016-11-05 22:09:58 +0100

    that's you that's how dumb you sound

--- src/Game.java
+++ src/Game.java
@@ -3,7 +3,7 @@ public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		board bd = new board(); /* let's kill it anyway */
+		board bd = new board(); /* claoker-san was here */
 	}
 
 }
